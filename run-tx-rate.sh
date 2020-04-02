963d90c4207b905069ebbcaeb96357f910884ebe
Alexey Stroganov <alexey2k@gmail.com>
2020-04-02 16:31:04 -0400

    ongoing results

---
+++ run-tx-rate.sh
@@ -0,0 +1,25 @@
+runid="2"
+RUNDIR=res-tpcc-GR-5nodes-1writer-BP25G-txrate3100-$runid
+#for i in 16 64 128 256
+#for i in 1 2 4 8 16 64 128 256
+for i in 100
+do
+     	OUTDIR=$RUNDIR/
+        mkdir -p $OUTDIR
+
+        time=1800
+	./tpcc.lua --mysql-host=172.16.0.12 --mysql-user=sbtest --mysql-password=sbtest --mysql-db=sbtest --time=$time --threads=$i --report-interval=1 --tables=10 --scale=100 --trx_level=RR  --db-driver=mysql --report_csv=yes --mysql-ignore-errors=3100,3101,1213,1180 run |  tee -a $OUTDIR/res_thr$i.txt
+
+	sleep 30
+	done
+
+for i in 64
+do
+     	OUTDIR=$RUNDIR/
+        mkdir -p $OUTDIR
+
+        time=1800
+	./tpcc.lua --mysql-host=172.16.0.12 --mysql-user=sbtest --mysql-password=sbtest --mysql-db=sbtest --time=$time --rate=3100 --threads=$i --report-interval=1 --tables=10 --scale=100 --trx_level=RR  --db-driver=mysql --report_csv=yes --mysql-ignore-errors=3100,3101,1213,1180 run |  tee -a $OUTDIR/res_thr$i.txt
+
+	sleep 30
+	done
