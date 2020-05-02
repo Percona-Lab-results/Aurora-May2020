58f9e86c0e08ded2bcec441ca01e6027fb11a829
Ubuntu <ubuntu@ip-172-31-11-49.us-east-2.compute.internal>
2020-05-02 11:33:33 +0000

    Aurora results

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 runid="1"
-RUNDIR=res-tpcc-PXC8-3nodes-2writer-BP25G-$runid
+RUNDIR=res-tpcc-Aurora-3nodes-5tables-db.r5.xlarge-$runid
 #for i in 16 64 128 256
 #for i in 1 2 4 8 16 64 128 256
 for i in 100 1 2 4 8 16 64 128 256
@@ -9,7 +9,7 @@ do
         mkdir -p $OUTDIR
 
         time=1800
-	./tpcc.lua --mysql-host=172.16.0.12,172.16.0.13 --mysql-user=sbtest --mysql-password=sbtest --mysql-db=sbtest --time=$time --threads=$i --report-interval=1 --tables=10 --scale=100 --trx_level=RR  --db-driver=mysql --report_csv=yes --mysql-ignore-errors=3100,3101,1213,1180 run |  tee -a $OUTDIR/res_thr$i.txt
+	./tpcc.lua --mysql-host=dbrest-cluster.cluster-chnjcb9bgxs7.us-east-2.rds.amazonaws.com --mysql-user=admin --mysql-password=Hi8KSsEA2YVsD --mysql-db=sbtest --time=$time --threads=$i --report-interval=1 --tables=5 --scale=100 --trx_level=RR  --db-driver=mysql --report_csv=yes --mysql-ignore-errors=3100,3101,1213,1180 run |  tee -a $OUTDIR/res_thr$i.txt
 
 	sleep 30
 	done
