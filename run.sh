6652fed689b4e6833420feffccc57c994d70b88b
Ubuntu <ubuntu@ip-172-31-27-36.us-east-2.compute.internal>
2020-05-12 16:31:12 +0000

    RDS results

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 runid="1"
-RUNDIR=res-tpcc-PXC-Operator-3nodes-10tables-r5.xlarge-try2-$runid
+RUNDIR=res-tpcc-RDS-3nodes-5tables-r5.xlarge-$runid
 #for i in 16 64 128 256
 #for i in 1 2 4 8 16 64 128 256
 for i in 100 1 2 4 8 16 64 128 256
@@ -9,7 +9,7 @@ do
         mkdir -p $OUTDIR
 
         time=1800
-	./tpcc.lua --mysql-host=192.168.168.164 --mysql-user=root --mysql-password=root_password --mysql-db=sbtest --time=$time --threads=$i --report-interval=1 --tables=10 --scale=100 --trx_level=RR  --db-driver=mysql --report_csv=yes --mysql-ignore-errors=3100,3101,1213,1180 run |  tee -a $OUTDIR/res_thr$i.txt
+	./tpcc.lua --mysql-host=db5.chnjcb9bgxs7.us-east-2.rds.amazonaws.com --mysql-user=admin --mysql-password=Zg8akTHGruktY --mysql-db=sbtest --time=$time --threads=$i --report-interval=1 --tables=5 --scale=100 --trx_level=RR  --db-driver=mysql --report_csv=yes --mysql-ignore-errors=3100,3101,1213,1180 run |  tee -a $OUTDIR/res_thr$i.txt
 
 	sleep 30
 	done
